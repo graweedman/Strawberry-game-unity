951142ebd49b3d34dbdcb52db4b6e6ca813e7790
graweedman <niba12ww@gmail.com>
2020-09-23 18:51:01 +0300

    Unity
    Initial Project

---
+++ Assets/GameEvents.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents current;
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    public event Action onDoorwayTriggerEnter;
+    public void DoorwayTriggerEnter()
+    {
+        if (onDoorwayTriggerEnter != null)
+        {
+            onDoorwayTriggerEnter();
+        }
+    }
+}
