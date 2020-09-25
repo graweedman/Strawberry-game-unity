ac31b20537ed8aa28ad5dd6e109a11a727888ef6
graweedman <niba12ww@gmail.com>
2020-09-25 14:40:22 +0300

    GameUpdate
    Added Settings menu
    
    PlayerMovement initialized
    
    Player Animations Added
    
    Playing area Has been Made
    
    Started WallJump function(WIP)

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Game Started");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public Animator animator;
+    public Rigidbody2D player;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        
+        
+        float Yvelocity = player.velocity.y;
+        animator.SetFloat("Yvelocity", Yvelocity);
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+            animator.SetBool("isgrounded", controller.getGrounded());
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
